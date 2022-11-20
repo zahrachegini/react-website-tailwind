3b25fae2b655efaf13a59c5befd9266e6d4a3084
unknown <z.chegini@falnic.com>
2022-11-21 00:10:50 +0330

    Smooth Scroll

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="w-full my-32">
+    <div className="w-full my-32" name="aboutUs">
       <div className="max-w-[1240px] mx-auto">
         <div className="text-center">
           <h2 className="text-5xl font-bold">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -7,17 +8,39 @@ const Navbar = () => {
     setNav(!nav);
   };
 
+  const handleClose = () => setNav(!nav);
+
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex text-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND</h1>
           <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>AboutUs</li>
-            <li>Support</li>
-            <li>Platform</li>
-            <li>Pricing</li>
+            <li>
+              <Link to="home" smooth={true} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="aboutUs" smooth={true} offset={-200} duration={500}>
+                AboutUs
+              </Link>
+            </li>
+            <li>
+              <Link to="support" smooth={true} offset={-50} duration={500}>
+                Support
+              </Link>
+            </li>
+            <li>
+              <Link to="platform" smooth={true} offset={-100} duration={500}>
+                Platform
+              </Link>
+            </li>
+            <li>
+              <Link to="pricing" smooth={true} offset={-50} duration={500}>
+                Pricing
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
@@ -31,11 +54,56 @@ const Navbar = () => {
         </div>
       </div>
       <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
-        <li className="border-b-2 border-zinc-300  w-full">Home</li>
-        <li className="border-b-2 border-zinc-300  w-full">AboutUs</li>
-        <li className="border-b-2 border-zinc-300  w-full">Support</li>
-        <li className="border-b-2 border-zinc-300  w-full">Platform</li>
-        <li className="border-b-2 border-zinc-300  w-full">Pricing</li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link onClick={handleClose} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="aboutUs"
+            smooth={true}
+            offset={-200}
+            duration={500}
+          >
+            About
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="support"
+            smooth={true}
+            offset={-50}
+            duration={500}
+          >
+            Support
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="platform"
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Platforms
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="pricing"
+            smooth={true}
+            offset={-50}
+            duration={500}
+          >
+            Pricing
+          </Link>
+        </li>
+
         <div className="flex flex-col my-4">
           <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
             Sign In
